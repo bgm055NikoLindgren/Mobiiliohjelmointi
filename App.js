6313718150f8dce32dbcf0d04665ec48b6bf2ff8
Niko Lindgren <bgm055@myy.haaga-helia.fi>
2021-11-01 23:19:47 +0200

    Calculator

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Button, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState('');
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ alignItems: 'center' }}>
+
+        <Text>Result: {result}</Text>
+
+        <TextInput
+          keyboardType='numeric'
+          style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={number1 => setNumber1(number1)}
+          value={number1}
+        />
+        <TextInput keyboardType='numeric'
+          style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={number2 => setNumber2(number2)}
+          value={number2}
+        />
+      </View>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
+        <Button
+          onPress={() => setResult(number1 - number2)}
+          title="-"
+        />
+        <Button
+          onPress={() => setResult(+number1 + +number2)}
+          title="+"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
 
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#A4EEFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
