f6ad9213c558fb386ef7cf172c81807410e455c3
Niko Lindgren <bgm055@myy.haaga-helia.fi>
2021-11-02 00:30:04 +0200

    shoppingList

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+
+
 
 export default function App() {
+
+  const [text, setText] = useState('');
+  const[data, setData] = useState([]);
+
+  const addButton = () => {
+    setData([...data, {key:text }]);
+    setText('');
+  }
+
+  const clearButton = () => {
+    setData('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} />
+
+      <View style={styles.button}>
+      <Button onPress={addButton} title="ADD" />
+      <Button onPress={clearButton} title="CLEAR" />
+      </View>
+
+      <Text >Shopping List</Text>
+      <FlatList 
+      data = {data}
+      renderItem={({item}) =>
+
+      <Text>{item.key}</Text>}
+
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +48,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    marginTop: 250,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1 
+  },
+  button: {
+    flexDirection: "row",
+  }
 });
